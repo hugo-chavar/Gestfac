04d05ca631f5f6d57a0441294ae6257c9c20a515
Hugo Chavar <hchavar@afip.gob.ar>
2021-12-20 01:11:47 -0300

    Added Add Product View

--- Gestfac/App.xaml.cs
+++ Gestfac/App.xaml.cs
@@ -23,7 +23,7 @@ namespace Gestfac
             {
                 catalog.AddProduct(new Product() { Id = 1, CurrentPrice = 12.5, Description = "Tornillo", ExternalId = "AAA111" });
                 catalog.AddProduct(new Product() { Id = 2, CurrentPrice = 34.2, Description = "Arandela para PVC", ExternalId = "BBB222" });
-                catalog.AddProduct(new Product() { Id = 3, CurrentPrice = 7.0, Description = "Pegamento PVC", ExternalId = "BBB222" });
+                catalog.AddProduct(new Product() { Id = 3, CurrentPrice = 7.0, Description = "Pegamento PVC", ExternalId = "CCC333" });
 
                 IEnumerable<Product> products = catalog.GetProductsByDescription("PVC");
 
